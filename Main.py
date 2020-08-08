c41e030f3d17e60b1b0898062ca90c2ec1a3b680
mihikabirmiwal <mihikabirmiwal@gmail.com>
2020-08-08 17:25:22 -0500

    Validates all inputs

--- Main.py
+++ Main.py
@@ -28,31 +28,50 @@ def viewForParticularDay(court_num, date):
     try:
         inputDate = datetime.date.fromisoformat(date)
     except:
-        return render_template('error.html', invalidDate=date)
+        return render_template('error.html', invalidDate=date, court=court_num)
     if (court_num not in reservations):
-        return render_template('error.html', invalidCourt=str(court_num))
+        return render_template('error.html', invalidCourt=str(court_num), todayDateAsString=date)
     pDay = inputDate-datetime.timedelta(days=1)
     nDay = inputDate+datetime.timedelta(days=1)  
     return render_template('view.html', daySched=reservations[court_num].get(inputDate,{}), day=inputDate, num=court_num, prevDay=pDay, nextDay=nDay)  #sends the dictionary for the day's reservations  
 
-@app.route('/view/court/<int:court_num>')
+@app.route('/view/court/<int:court_num>', methods=['GET'])
 def viewForToday(court_num):
-    todayDate = datetime.date.today() 
+    todayDate = datetime.date.today()
     todayDateString = todayDate.isoformat()  
+    if court_num not in reservations:
+        return render_template('error.html', invalidCourt=str(court_num), todayDateAsString=todayDateString)
     return viewForParticularDay(court_num, todayDateString)
 
 #this is where the reservation is made, not at view.html
 #receives the post request
 @app.route('/view/court/<int:court_num>/<date>/hour/<int:hour>', methods=['POST'])
-def reserveCourt(court_num, date, hour):
+def reserveCourt(court_num, date, hour):     
     try: 
         inputDate = datetime.date.fromisoformat(date)
     except:
-        return render_template('error.html', invalidDate=date)
+        return render_template('error.html', invalidDate=date, court=court_num)
+    if not request.form['user']:
+        return render_template('error.html', invalidName='bad', court=court_num, todayDateAsString=date)  
     if (court_num not in reservations):
-        return render_template('error.html', invalidCourt=court_num)
-    else:
-        if inputDate not in reservations[court_num]:
-            reservations[court_num][inputDate]={}
-        reservations[court_num][inputDate][hour] = request.form['user']
-        return viewForParticularDay(court_num, date)
+        return render_template('error.html', invalidCourt=str(court_num), todayDateAsString=date)
+    if inputDate not in reservations[court_num]:
+        reservations[court_num][inputDate]={}
+    reservations[court_num][inputDate][hour] = request.form['user']
+    return viewForParticularDay(court_num, date)
+
+#if i changed the cancel part of this url to be view, how would the view.html know which method to go to?
+@app.route('/cancel/court/<int:court_num>/<date>/hour/<int:hour>', methods=['POST'])
+def deleteReservation(court_num, date, hour):
+    try: 
+        inputDate = datetime.date.fromisoformat(date)
+    except:
+        return render_template('error.html', invalidDate=date, court=court_num)
+    if court_num not in reservations:
+        return render_template('error.html', invalidCourt=str(court_num), todayDateAsString=date)
+    if inputDate not in reservations[court_num]:
+        return render_template('error.html', invalidDate=date, court=court_num)
+    if hour not in reservations[court_num][inputDate]:
+        return render_template('error.html', invalidHour=str(hour), todayDateAsString=date, court=court_num)
+    del reservations[court_num][inputDate][hour]
+    return viewForParticularDay(court_num, date)
